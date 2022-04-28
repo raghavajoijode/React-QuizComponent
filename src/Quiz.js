7b136f2f1605f83bfb61f11b86b654a097965f4f
Raghava Joijode <Raghava.Joijode@contracted.pmi.com>
2022-04-28 14:08:07 +0530

    Module 5

--- src/Quiz.js
+++ src/Quiz.js
@@ -12,6 +12,11 @@ class Quiz extends Component {
             quiz_position: 1
         }
     }
+    showNextQuestion() {
+        this.state = {
+            quiz_position: this.state.quiz_position + 1
+        }
+    }
 
     render() {
         const isQuizEnd = (this.state.quiz_position - 1) === quizData.quiz_questions.length
@@ -20,6 +25,7 @@ class Quiz extends Component {
                 {isQuizEnd ?
                     <QuizEnd /> :
                     <QuizQuestion
+                        showNextQuestionHandler={this.showNextQuestion.bind(this)}
                         quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]} />}
             </div>
         )
