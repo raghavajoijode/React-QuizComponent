aea1af02bea6d7048a252b7f23656467aa445a84
Raghava Joijode <Raghava.Joijode@contracted.pmi.com>
2022-04-28 13:03:55 +0530

    Module 2

--- src/Quiz.js
+++ src/Quiz.js
@@ -1,4 +1,6 @@
 import React, { Component } from "react";
+import QuizQuestion from "./QuizQuestion"
+
 let quizData = require("./quiz_data.json")
 
 class Quiz extends Component {
@@ -14,9 +16,8 @@ class Quiz extends Component {
         let firstQuiz = quizData.quiz_questions[0]
         return (
             <div>
-                <div className="QuizQuestion">
-                    {firstQuiz.instruction_text}
-                </div>
+                <QuizQuestion
+                    quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]} />
             </div>
         )
     }
